3acebc26b93ed984160d7df4bb6375e21161998d
rohitdshpnd8@gmail.com <#Chess111>
2022-10-21 00:14:05 +0530

    code for random port

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.net.HttpURLConnection;
@@ -8,12 +9,14 @@ import java.net.URL;
 
 @SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.RANDOM_PORT)
 class DemoApplicationTests {
+	@Value("${local.server.port}")
+	int port;
 
 	@Test
 	void contextLoads() {
 		try {
 
-			URL url = new URL("http://localhost:8081/home");
+			URL url = new URL("http://localhost:"+port+"/home");
 			HttpURLConnection con = (HttpURLConnection) url.openConnection();
 			con.setRequestMethod("GET");
 			int responseCode = con.getResponseCode();
